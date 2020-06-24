a7841fe5f9a2882379d19974846bc29645fe7cc1
Luke Inger <luke.inger@icloud.com>
2020-06-24 18:59:27 +0100

    first commit

---
+++ Extension/Script.swift
@@ -0,0 +1,13 @@
+//
+//  Script.swift
+//  Extension
+//
+//  Created by Luke Inger on 24/06/2020.
+//  Copyright © 2020 Luke Inger. All rights reserved.
+//
+
+import UIKit
+
+class Script: NSObject {
+
+}
